424f4c7142095536caa4e3191b0f66ca169fe11a
Tory Stodard <tory.stodard@gmail.com>
2023-08-13 20:43:12 -0600

    completed exercise 05.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let newSum = 0;
+  let firstNumber;
+  let secondNumber;
+  if (typeof num1 != 'number' || typeof num2 != 'number') {
+    return 'ERROR';
+  } else if (num1 < 0 || num2 < 0) {
+    return 'ERROR';
+  } else if (num1 < num2) {
+    firstNumber = num1;
+    secondNumber = num2;
+  } else if (num1 > num2) {
+    firstNumber = num2;
+    secondNumber = num1;
+  }
+  for (let i = firstNumber; i <= secondNumber; i++) {
+    newSum += i;
+  }
 
+  return newSum;
 };
 
 // Do not edit below this line
